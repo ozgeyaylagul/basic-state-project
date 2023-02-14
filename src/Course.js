78aad5099186cc70486696c737881204976cffc6
ozgeyaylagul <103510452+ozgeyaylagul@users.noreply.github.com>
2023-02-15 01:29:12 +0200

    css additions

--- src/Course.js
+++ src/Course.js
@@ -1,22 +1,24 @@
-import Angular from './images/angular.jpg';
-import Bootstrap from './images/bootstrap5.png';
-import Ccsharp from './images/ccsharp.png';
-import Frontend from './images/kompleweb.jpg';
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Ccsharp from "./images/ccsharp.png";
+import Frontend from "./images/kompleweb.jpg";
+import "./Course.css";
 
-const courseMap ={
-    Angular,
-    Bootstrap,
-    Ccsharp,
-    Frontend,
-}
-function Course({courseName}) {
-    // console.log(Angular);
-    // console.log(courseName);
-    console.log(courseMap[courseName])
-    return <div>
-        <img src={courseMap[courseName]} alt="course" />
-    </div>;
+const courseMap = {
+  Angular,
+  Bootstrap,
+  Ccsharp,
+  Frontend,
+};
+function Course({ courseName }) {
+  // console.log(Angular);
+  // console.log(courseName);
+  console.log(courseMap[courseName]);
+  return (
+    <div className="courseDiv">
+      <img className="course" src={courseMap[courseName]} alt="course" />
+    </div>
+  );
 }
 
 export default Course;
-
